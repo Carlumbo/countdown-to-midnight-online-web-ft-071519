828365780d14d753a0203f49616c33d638f4af58
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 19:14:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(count)
-  count = 11 
+  number = 11 
   while count > 0 
-    puts "#{count} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     count -= 1 
   end 
   puts "happy new year!".upcase
